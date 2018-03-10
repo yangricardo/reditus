d5be6559d66b1e45be48117e5220f1925cdcde65
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2018-03-10 16:42:40 -0300

    refatorando leitura de arquivos

--- search/apps.py
+++ search/apps.py
@@ -6,6 +6,7 @@ import os
 from os import listdir
 from os.path import isfile, join
 import re
+import sys
 
 class SearchConfig(AppConfig):
     name = 'search'
@@ -15,51 +16,65 @@ class SearchConfig(AppConfig):
     '''
         process_dict = {
             cod_proc : {
-                'data' : df
-                'serventia' : serventia
-                'comarca' : comarca
+                'cod_process':str(cod_process) ,
+                'hash_processes':[ str(hash_processes) ], 
+                'serventia' : str(serventia)
+                'comarca' : str(comarca)
             }
         }
-        key => cod_proc
-        value => {'data','serventia','comarca'}
     '''
 
     def build_process_dict(self):
         regex_file = re.compile(r"similar_to_([0-9A-Z]+)\.csv")
-        path = os.path.join(settings.BASE_DIR, 'static/data/similar_const_test/'),
+        path = os.path.join(settings.BASE_DIR, 'static/data/similar_data/'),
         #cria a lista com o nome de todos os arquivos do diretorio que se adequam ao regex_file
         onlyfiles = [ f for f in listdir(path[0]) if isfile(join(path[0], f)) and regex_file.match(f) ]
+        print("Lendo arquivos de similares do diretorio: {}".format(path[0]))
+        # for f in onlyfiles:
+        #     data = pd.read_csv(path[0] + f, sep=";" ,encoding = 'latin1')
+            
+        #     data = data[~data['sentenca'].str.contains('Homologo|HOMOLOGO|homologo|homologa-se|Homologa-se|Projeto em Revisão')]
+        #     data = data[data['similar_processo'] != data['processo']]
+            
+        #     data = data.reset_index(drop=True)
+
+        #     cod_process = data['processo'][0]
+
+        #     if cod_process not in self.process_dict:
+        #         self.process_dict.update({cod_process : {'data':data, 'similar_processo':{} }})
+        #     else:
+        #         self.process_dict.get(cod_process).get('data').append(data)
+
+        #     for index, row in self.process_dict.get(cod_process).get('data').iterrows():
+        #         similar = re.search(r"[0-9]{4}\.[0-9]{3}\.[0-9]{6}-[0-9]",row['similar_processo']).group(0)
+
+        #         if similar not in self.process_dict.get(cod_process).get('similar_processo'):
+        #             similar_url = "http://gedweb.tjrj.jus.br/gedcacheweb/default.aspx?gedid="+row['similar_file']
+        #             self.process_dict.get(cod_process).get('similar_processo').update({similar : (similar,similar_url,row['similaridade']) })
+
+        #     process_url = "http://gedweb.tjrj.jus.br/gedcacheweb/default.aspx?gedid="+re.search(r"similar_to_([0-9A-Z]+)\.csv",path[0] + f).group(1)
+        #     self.process_dict.get(cod_process).update({'process_url':process_url})
 
         for f in onlyfiles:
             data = pd.read_csv(path[0] + f, sep=";" ,encoding = 'latin1')
-            
-            data = data[~data['sentenca'].str.contains('Homologo|HOMOLOGO|homologo|homologa-se|Homologa-se|Projeto em Revisão')]
-            data = data[data['similar_processo'] != data['processo']]
-            
-            data = data.reset_index(drop=True)
-
             cod_process = data['processo'][0]
+            hash_process = regex_file.match(f).group(1)
 
             if cod_process not in self.process_dict:
-                self.process_dict.update({cod_process : {'data':data, 'similar_processo':{} }})
+                self.process_dict.update({
+                    cod_process: {
+                        'cod_process':cod_process ,
+                        'hash_processes':[hash_process], 
+                        } 
+                    })
             else:
-                self.process_dict.get(cod_process).get('data').append(data)
-
-            for index, row in self.process_dict.get(cod_process).get('data').iterrows():
-                similar = re.search(r"[0-9]{4}\.[0-9]{3}\.[0-9]{6}-[0-9]",row['similar_processo']).group(0)
-
-                if similar not in self.process_dict.get(cod_process).get('similar_processo'):
-                    similar_url = "http://gedweb.tjrj.jus.br/gedcacheweb/default.aspx?gedid="+row['similar_file']
-                    self.process_dict.get(cod_process).get('similar_processo').update({similar : (similar,similar_url,row['similaridade']) })
-
-            process_url = "http://gedweb.tjrj.jus.br/gedcacheweb/default.aspx?gedid="+re.search(r"similar_to_([0-9A-Z]+)\.csv",path[0] + f).group(1)
-            self.process_dict.get(cod_process).update({'process_url':process_url})
-
+                self.process_dict.get(cod_process).get('hash_processes').append(hash_process)
 
         regex_file = re.compile(r"elasticinput_([0-9A-Z])+\.csv")
-        path = os.path.join(settings.BASE_DIR, 'static/data/data_tj/'),
+        path = os.path.join(settings.BASE_DIR, 'static/data/elastic_data/'),
         #cria a lista com o nome de todos os arquivos do diretorio que se adequam ao regex_file
-        onlyfiles = [ f for f in listdir(path[0]) if isfile(join(path[0], f)) and regex_file.match(f) ]                
+        onlyfiles = [ f for f in listdir(path[0]) if isfile(join(path[0], f)) and regex_file.match(f) ]       
+        print("Lendo metadados do diretorio: {}".format(path[0]))         
         for f in onlyfiles:
             #le os dados do arquivo csv
             df = pd.read_csv(path[0] + f, sep=";" ,encoding = 'latin1') 
@@ -75,6 +90,8 @@ class SearchConfig(AppConfig):
                             'comarca': comarca,
                         }
                     )
+
+        print(self.process_dict)
     #end of build_process_dict    
 
     def ready(self):
@@ -82,3 +99,4 @@ class SearchConfig(AppConfig):
         self.build_process_dict()
         print('Códigos de Processos Carregados:')
         print(self.process_dict.keys())
+        print("{} bytes ocupados em memória".format(sys.getsizeof(self.process_dict)))
--- search/views.py
+++ search/views.py
@@ -5,6 +5,9 @@ from .apps import SearchConfig as sc
 import pandas as pd
 import numpy as np
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+import os
+from reditus import settings
+from os.path import isfile, join
 import re
 
 
@@ -32,43 +35,66 @@ def index_view(request):
 
 
 def process_view(request,cod, index):
-    try:
-        process_data = search_process_test(cod)
-        
-        if bool(process_data):
-            serventia = process_data.get('serventia')
-            comarca = process_data.get('comarca')
-            df = process_data.get('data')
-            process = df['processo'][int(index)]
-            process_contestacao_url = process_data.get('process_url')
-            sentence = df['sentenca'][int(index)]
-            similar = re.search(r"\d{4}\.\d{3}\.\d{6}-\d",df['similar_processo'][int(index)]).group(0)
-            author = re.search(r"(autor|Autor|AUTOR|Autora|autora|AUTORA)(\s*:\s*)(\w.+)+",sentence).group(3) if re.search(r"(autor|Autor|AUTOR|Autora|autora|AUTORA)(\s*:\s*)(\w.+)+[^\n]",sentence) else ""
-            similar_atual = re.search(r"\d{5,7}-\d{2}\.\d{4}\.\d\.\d{2}\.\d{4}",sentence).group(0) if re.search(r"\d{5,7}-\d{2}\.\d{4}\.\d\.\d{2}\.\d{4}",sentence) else ""
-            reu = re.search(r"(reu|Reu|Réu|réu|REU|RÉU|RÉ|Ré|ré)(\s*:\s*)(\w.+)+",sentence).group(3) if re.search(r"(reu|Reu|Réu|réu|REU|RÉU|RÉ|Ré|ré)(\s*:\s*)(\w.+)+[^\n]",sentence) else ""
-            similar_contestacao_url = "http://gedweb.tjrj.jus.br/gedcacheweb/default.aspx?gedid="+df['similar_file'][int(index)]
-            process_movimento_url = "http://www4.tjrj.jus.br/consultaProcessoWebV2/consultaMov.do?v=2&numProcesso="+process+"&acessoIP=internet&tipoUsuario="
-            similar_movimento_url = "http://www4.tjrj.jus.br/consultaProcessoWebV2/consultaMov.do?v=2&numProcesso="+similar+"&acessoIP=internet&tipoUsuario="
-            similaridade = df['similaridade'][int(index)]
+    # try:
+    #     process_data = search_process_test(cod)
+    #     similar_data = get_similar_data(process_data)
+    #     # if bool(process_data):
+        #     serventia = process_data.get('serventia')
+        #     comarca = process_data.get('comarca')
+        #     df = get_similar_data
+        #     process = df['processo'][int(index)]
+        #     process_contestacao_url = process_data.get('process_url')
+        #     sentence = df['sentenca'][int(index)]
+        #     similar = re.search(r"\d{4}\.\d{3}\.\d{6}-\d",df['similar_processo'][int(index)]).group(0)
+        #     author = re.search(r"(autor|Autor|AUTOR|Autora|autora|AUTORA)(\s*:\s*)(\w.+)+",sentence).group(3) if re.search(r"(autor|Autor|AUTOR|Autora|autora|AUTORA)(\s*:\s*)(\w.+)+[^\n]",sentence) else ""
+        #     similar_atual = re.search(r"\d{5,7}-\d{2}\.\d{4}\.\d\.\d{2}\.\d{4}",sentence).group(0) if re.search(r"\d{5,7}-\d{2}\.\d{4}\.\d\.\d{2}\.\d{4}",sentence) else ""
+        #     reu = re.search(r"(reu|Reu|Réu|réu|REU|RÉU|RÉ|Ré|ré)(\s*:\s*)(\w.+)+",sentence).group(3) if re.search(r"(reu|Reu|Réu|réu|REU|RÉU|RÉ|Ré|ré)(\s*:\s*)(\w.+)+[^\n]",sentence) else ""
+        #     similar_contestacao_url = "http://gedweb.tjrj.jus.br/gedcacheweb/default.aspx?gedid="+df['similar_file'][int(index)]
+        #     process_movimento_url = "http://www4.tjrj.jus.br/consultaProcessoWebV2/consultaMov.do?v=2&numProcesso="+process+"&acessoIP=internet&tipoUsuario="
+        #     similar_movimento_url = "http://www4.tjrj.jus.br/consultaProcessoWebV2/consultaMov.do?v=2&numProcesso="+similar+"&acessoIP=internet&tipoUsuario="
+        #     similaridade = df['similaridade'][int(index)]
 
-            similar_processes = list(process_data.get('similar_processo').items())
+        #     similar_processes = list(process_data.get('similar_processo').items())
 
             
-            pages = df['processo'].count()-1
+        #     pages = df['processo'].count()-1
             
-            has_previous = True if int(index) > 0 else False
-            has_next = True if int(index) < pages else False
-            previousIndex = int(index)-1 if int(index) > 0 else 0
-            nextIndex = int(index)+1 if int(index) < pages else pages
-            #return render(request, 'search/sentenca.html', {'process': process,'sentence':sentence,'similar':similar,'author':author,'reu':reu, 'pages':pages, 'previousindex':previousIndex,'nextindex':nextIndex })
-            return render(request, 'search/sentenca.html', {'cod':cod, 'index':index ,'process': process,'process_contestacao_url':process_contestacao_url,'sentence':sentence,'similar':similar,'similaridade':similaridade,'similar_atual':similar_atual,'similar_processes':similar_processes,'author':author,'reu':reu,'similar_contestacao_url':similar_contestacao_url,'similar_movimento_url':similar_movimento_url,'process_movimento_url':process_movimento_url,'serventia':serventia,'comarca':comarca,'pages':pages, 'previousindex':previousIndex,'nextindex':nextIndex,'has_previous':has_previous,'has_next':has_next  })
-        
-        return render(request, 'search/sentenca.html')
-    except:
-        error = True
-        return render(request, 'search/sentenca.html',{'error':error } )
+        #     has_previous = True if int(index) > 0 else False
+        #     has_next = True if int(index) < pages else False
+        #     previousIndex = int(index)-1 if int(index) > 0 else 0
+        #     nextIndex = int(index)+1 if int(index) < pages else pages
+        #     #return render(request, 'search/sentenca.html', {'process': process,'sentence':sentence,'similar':similar,'author':author,'reu':reu, 'pages':pages, 'previousindex':previousIndex,'nextindex':nextIndex })
+        #     return render(request, 'search/sentenca.html', {'cod':cod, 'index':index ,'process': process,'process_contestacao_url':process_contestacao_url,'sentence':sentence,'similar':similar,'similaridade':similaridade,'similar_atual':similar_atual,'similar_processes':similar_processes,'author':author,'reu':reu,'similar_contestacao_url':similar_contestacao_url,'similar_movimento_url':similar_movimento_url,'process_movimento_url':process_movimento_url,'serventia':serventia,'comarca':comarca,'pages':pages, 'previousindex':previousIndex,'nextindex':nextIndex,'has_previous':has_previous,'has_next':has_next  })
+    #     return render(request, 'search/sentenca.html')
+    # except:
+    #     error = True
+    #     return render(request, 'search/sentenca.html',{'error':error } )
+
+    process_data = search_process_test(cod)
+    similar_data = get_similar_data(process_data.get('hash_processes'))
+
+    return render(request,'search/sentenca.html')
 
 
+
+def get_similar_data(hash_processes):
+    regex_file = re.compile(r"similar_to_([0-9A-Z]+)\.csv")
+    path = os.path.join(settings.BASE_DIR, 'static/data/similar_data/'),
+
+    process_data = pd.DataFrame()
+
+    for hash_process in hash_processes:
+        csv = join(path[0], "similar_to_{}.csv".format(hash_process))
+        data = pd.read_csv(csv, sep=";" ,encoding = 'latin1')
+        data = data[~data['sentenca'].str.contains('Homologo|HOMOLOGO|homologo|homologa-se|Homologa-se|Projeto em Revisão')]
+        data = data[data['similar_processo'] != data['processo']]
+        process_data = data if process_data.empty else process_data.append(data)
+
+    process_data = process_data.sort_values(by=['similaridade'],ascending=False)
+    process_data = process_data.reset_index(drop=True)
+    print(process_data)
+
+    return process_data
 
 class Process(object):
     cod = ""
