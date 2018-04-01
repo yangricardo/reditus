51ce5dd12e9fbf0ea3ce594fe859cb8b434c2a46
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2018-04-01 14:55:01 -0300

    identificando que arquivo similar ja foi cadastrado

--- search/management/commands/similarsrebase.py
+++ search/management/commands/similarsrebase.py
@@ -20,7 +20,7 @@ class Command(BaseCommand):
 
     def similarrebase(self):
         regex_file = re.compile(r"similar_to_([0-9A-Z]+)\.csv")
-        path = os.path.join(settings.BASE_DIR, 'static/data/similar_data/similares_const'),
+        path = os.path.join(settings.BASE_DIR, 'static/data/similar_data/similares_const/'),
         #cria a lista com o nome de todos os arquivos do diretorio que se adequam ao regex_file
         onlyfiles = [ f for f in listdir(path[0]) if isfile(join(path[0], f)) and regex_file.match(f) ]
         print("Lendo arquivos de similares do diretorio: {}".format(path[0]))
